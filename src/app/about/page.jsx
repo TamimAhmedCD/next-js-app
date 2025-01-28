78fdb3f39e73f46c21da91208d4281d0f5af5e3a
Tamim Ahmed <tamim20072@gmail.com>
2025-01-28 14:20:00 +0600

    about page route done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+    return (
+        <div className="text-white w-11/12 md:w-10/12 mx-auto">
+            <p className='font-bold text-3xl'>About Page</p>
+        </div>
+    );
+};
+
+export default AboutPage;
