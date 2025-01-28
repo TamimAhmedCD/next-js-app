47910428b1b951d76d34b706c7a474d639c61c67
Tamim Ahmed <tamim20072@gmail.com>
2025-01-29 00:36:15 +0600

    Navbar Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-
-      <nav className="sticky top-0 flex justify-between items-center gap-5 text-white w-11/12 md:w-10/12 mx-auto mt-6">
-        {/* Logo */}
-        <div>
-          <h1 className="text-xl font-bold">LOGO</h1>
-        </div>
-        {/* Nav-list */}
-        <ul className="flex gap-3">
-            <li>Home</li>
-            <li>Services</li>
-            <li>About</li>
-        </ul>
-        {/* Button */}
-        <div>
-            <Button variant="secondary">Contact Me</Button>
-        </div>
-      </nav>
-
+    <nav className="sticky top-0 flex justify-between items-center gap-5 text-white w-11/12 md:w-10/12 mx-auto mt-6">
+      {/* Logo */}
+      <div>
+        <h1 className="text-xl font-bold">LOGO</h1>
+      </div>
+      {/* Nav-list */}
+      <ul className="flex gap-3">
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/services">
+          <li>Services</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+      </ul>
+      {/* Button */}
+      <div>
+        <Button variant="secondary">Contact Me</Button>
+      </div>
+    </nav>
   );
 };
 
