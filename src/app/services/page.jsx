7b801882d987b629f780b86280c6cc195432c625
Tamim Ahmed <tamim20072@gmail.com>
2025-01-28 14:16:41 +0600

    make services route

---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesPage = () => {
+    return (
+        <div className='text-white w-11/12 md:w-10/12 mx-auto'>
+            <p className='font-bold text-3xl'>Services Page</p>
+        </div>
+    );
+};
+
+export default ServicesPage;
